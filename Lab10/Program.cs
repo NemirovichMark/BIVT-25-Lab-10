a1d404bdff099dbf9da6d675835e022f0c50f02c
Maksonchik <maksonchik@Maksonchiks-MacBook-Air.local>
2026-04-20 10:35:43 +0300

    classwork 20.04

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Lab10
+﻿using Lab10.White;
+using Lab9.Purple;
+
+namespace Lab10
 {
     public class Program
     {
         public static void Main()
         {
+            var obj = new Lab9.Purple.Task1($"maksim fediarin");
+            var simpleSerializator = new WhiteTxtFileManager("Example1");
+            simpleSerializator.Serialize(obj);
+            // simpleSerializator = new WhiteTxtFileManager("Example2","txt");
+            // simpleSerializator.Serialize(new Lab9.Purple.Purple);
         }
     }
 }
