f4fa13a75ec0a41801aa32276ccefc2a6eb05e0a
Maksonchik <maksonchik@Maksonchiks-MacBook-Air.local>
2026-04-29 00:09:53 +0300

    update PurpleFileManager && Purple
    PurpleFileManager вроде как полностью готов а вот Purple еще доделывать надо

--- Lab10/Purple/Purple.cs
+++ Lab10/Purple/Purple.cs
@@ -1,6 +1,60 @@
-﻿namespace Lab10.Purple;
+namespace Lab10.Purple;
 
-public class Purple
+public class Purple<T> where T : Lab9.Purple.Purple
 {
+    private T[] _tasks;
 
+    public PurpleFileManager<T> Manager {get; private set;}
+    public T[] Tasks => _tasks;
+
+    public Purple (T[] tasks) => _tasks = tasks ?? Array.Empty<T>();
+    public Purple (PurpleFileManager<T> manager, T[]? tasks)
+    {
+        Manager = manager;
+        _tasks = tasks ?? Array.Empty<T>();
+    }
+    public Purple (T[] tasks, PurpleFileManager<T> manager)
+    {
+        Manager = manager;
+        _tasks = tasks ?? Array.Empty<T>();
+    }
+
+    public void Add (T task)
+    {
+        if (task == null) return;
+        Array.Resize(ref _tasks, Tasks.Length+1);
+        _tasks[^1] = task;
+    }
+    public void Add (T[] tasks)
+    {
+        foreach (var task in tasks) Add(task);
+    }
+
+    public void Remove(T task)
+    {
+        int ind_task_to_remove = Array.FindIndex(_tasks, t => t.ToString() == task.ToString());
+        if (ind_task_to_remove != -1)
+        {
+            T[] new_tasks = new T[_tasks.Length-1];
+            for (int i=0; i<new_tasks.Length; i++)
+            {
+                if (i < ind_task_to_remove) new_tasks[i] = _tasks[i];
+                else new_tasks[i] = _tasks[i+1];
+            }
+            _tasks = new_tasks;
+        }
+    }
+    public void Clear()
+    {
+        _tasks = Array.Empty<T>();
+        if (Manager != null && Directory.Exists(Manager.FolderPath))
+            Directory.Delete(Manager.FolderPath, true);
+    }
+
+    public void SaveTasks()
+    {
+        // ChangeFileName
+        // Serialize
+    }
+    
 }
--- Lab10/Purple/PurpleFileManager.cs
+++ Lab10/Purple/PurpleFileManager.cs
@@ -1,6 +1,21 @@
-﻿namespace Lab10.Purple;
+namespace Lab10.Purple;
 
-public class PurpleFileManager
+public abstract class PurpleFileManager<T> : MyFileManager, ISerializer<T>
+    where T : Lab9.Purple.Purple
 {
+    public PurpleFileManager(string name) : base(name)
+    {
+    }
 
+    public PurpleFileManager(
+        string name, 
+        string folderPath, 
+        string fileName, 
+        string fileExtension = "")
+        : base(name, folderPath, fileName, fileExtension)
+    {
+    }
+
+    public abstract T Deserialize();
+    public abstract void Serialize(T obj);
 }
