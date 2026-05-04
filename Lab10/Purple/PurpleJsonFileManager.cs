53f269066e35fcade4f693b95bd429a542f3e2f0
Maksonchik <maksonchik@Maksonchiks-MacBook-Air.local>
2026-05-04 14:25:11 +0300

    update PurpleJsonFileManager.cs

--- Lab10/Purple/PurpleJsonFileManager.cs
+++ Lab10/Purple/PurpleJsonFileManager.cs
@@ -1,4 +1,5 @@
 namespace Lab10.Purple;
+using Newtonsoft.Json.Linq;
 
 public class PurpleJsonFileManager<T> : PurpleFileManager<T>
     where T : Lab9.Purple.Purple
@@ -23,13 +24,39 @@ public class PurpleJsonFileManager<T> : PurpleFileManager<T>
     }
     
     public override void ChangeFileExtension(string new_extension) =>
-        base.ChangeFileExtension("txt");
+        base.ChangeFileFormat("json");
 
     public override void Serialize(T obj)
     {
+        if (obj == null) return;
+
+        JObject json_object = JObject.FromObject(obj);
+        json_object.Add("Type", obj.GetType().AssemblyQualifiedName);
+        string json_object_string = json_object.ToString();
+
+        Directory.CreateDirectory(FolderPath);
+        File.WriteAllText(FullPath,json_object_string);
     }
     public override T Deserialize()
     {
-        return null!;
+        if (!File.Exists(FullPath)) return null!;
+
+        string json_object_string = File.ReadAllText(FullPath);
+        JObject json_object = JObject.Parse(json_object_string);
+
+        string? json_object_type = json_object["Type"]?.ToString();
+        if (string.IsNullOrWhiteSpace(json_object_type)) return null!;
+
+        Type? object_type = Type.GetType(json_object_type);
+        if (object_type == null) return null!;
+
+        if (!typeof(T).IsAssignableFrom(object_type)) return null!;
+
+        json_object.Remove("Type");
+
+        object? obj = json_object.ToObject(object_type);
+        if (obj == null) return null!;
+
+        return (T)obj;
     }
 }
