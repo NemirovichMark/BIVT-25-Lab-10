e42cf4159ff24c5ffb824ad8f1a21de26d3dad8e
trefa <trefa@TREFAS-NOTE>
2026-05-05 19:20:18 +0300

    тест

---
+++ Lab10/Green/Green.cs
@@ -0,0 +1,63 @@
+﻿namespace green
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+
+    }
+    public class Green
+    {
+        protected GreenFileManager _manager;
+        protected Lab9.Green.Green[] _tasks;
+
+
+        public GreenFileManager Manager => _manager;
+        public Lab9.Green.Green[] Tasks => _tasks.Clone() as Lab9.Green.Green[];
+
+
+        public Green(Lab9.Green.Green[] tasks)
+        {
+            if (tasks != null)
+            {
+                _tasks = (Lab9.Green.Green[])tasks.Clone();
+            }
+            else
+            {
+                _tasks = new Lab9.Green.Green[0];
+            }
+
+        }
+        public Green(GreenFileManager manager, Lab9.Green.Green[] tasks = new Lab9.Green.Green[0])
+        {
+            _manager = manager;
+            if (tasks != null)
+            {
+                _tasks = (Lab9.Green.Green[])tasks.Clone();
+            }
+            else
+            {
+                _tasks = new Lab9.Green.Green[0];
+            }
+
+        }
+        public Green(Lab9.Green.Green[] tasks, GreenFileManager manager)
+        {
+            _manager = manager;
+            if(tasks != null)
+            {
+                _tasks = (Lab9.Green.Green[])tasks.Clone();
+            }
+            else
+            {
+                _tasks = new Lab9.Green.Green[0];
+            }
+        }
+        
+
+
+    }
+}
---
+++ Lab10/IFileLifeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10Test.Green
+{
+    internal interface IFileLifeController
+    {
+        public void CreateFile();
+        public void DeleteFile();
+
+        public void EditFile(string text);
+        public void ChangeFileExtension(string extension);
+
+    }
+}
