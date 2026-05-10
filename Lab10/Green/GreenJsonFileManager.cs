898fb0c2bef982b4288030c1c1c59cfcac775f38
Yokiow <Arishka.drachenko@mail.ru>
2026-05-10 23:37:09 +0300

    Add files via upload

---
+++ Lab10/Green/GreenJsonFileManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Lab10.Green
+{
+    public class GreenJsonFileManager: GreenFileManager
+    {
+        public GreenJsonFileManager(string name) : base(name)
+        {
+        }
+
+        public GreenJsonFileManager(string name, string folderPath, string fileName, string fileExtension = "json"): base(name, folderPath, fileName, fileExtension)
+        {
+        }
+
+        // Переопределяем EditFile (Десериализация -> Изменение -> Сериализация)
+        public override void EditFile(string newText)
+        {
+            var obj = Deserialize<Lab9.Green.Green>();
+            obj.ChangeText(newText);
+            Serialize(obj);
+        }
+        // Переопределяем смену расширения (только на json)
+        public override void ChangeFileExtension(string newExtension)
+        {
+            ChangeFileFormat("json");
+        }
+
+        // Обобщенный метод Serialize
+        public override void Serialize<T>(T obj) //// YTYTYTYTYT
+        {
+            if (obj == null) return;
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(obj, obj.GetType(), options);
+
+            if (json.StartsWith("{"))
+            {
+                json = json.Insert(1, $"\n  \"TypeName\": \"{obj.GetType().FullName}\",");
+            }
+            File.WriteAllText(FullPath, json);
+        }
+        public override T Deserialize<T>()
+        {
+            if (!File.Exists(FullPath)) return null;
+            string json = File.ReadAllText(FullPath);
+            using (JsonDocument doc = JsonDocument.Parse(json))
+            {
+                var root = doc.RootElement;
+                Type actualType = typeof(T);
+
+                if (root.TryGetProperty("TypeName", out var typeEl))
+                {
+                    string typeName = typeEl.GetString();
+                    if (!string.IsNullOrEmpty(typeName))
+                    {
+                        actualType = typeof(Lab9.Green.Green).Assembly.GetType(typeName) ?? typeof(T);
+                    }
+                }
+
+                var ctors = actualType.GetConstructors();
+                if (ctors.Length == 0) return null;
+
+                var ctor = ctors[0]; 
+                var pInfos = ctor.GetParameters();
+                object[] args = new object[pInfos.Length];
+
+                for (int i = 0; i < pInfos.Length; i++)
+                {
+                    string pName = pInfos[i].Name.ToLower();
+
+                    foreach (var prop in root.EnumerateObject())
+                    {
+                        string jsonPropName = prop.Name.ToLower();
+
+                        if (jsonPropName == pName || jsonPropName.Contains(pName))
+                        {
+                            if (pInfos[i].ParameterType == typeof(string))
+                            {
+                                args[i] = prop.Value.GetString();
+                            }
+                            else
+                            {
+                                args[i] = null;
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                T obj = (T)ctor.Invoke(args);
+
+                if (obj != null)
+                {
+                    var reviewMethod = obj.GetType().GetMethod("Review");
+                    if (reviewMethod != null)
+                    {
+                        reviewMethod.Invoke(obj, null);
+                    }
+                }
+                return obj;
+            }
+        }
+    }
+}
---
+++ Lab10/Green/GreenTxtFileManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10.Green
+{
+    public class GreenTxtFileManager : GreenFileManager
+    {
+        public GreenTxtFileManager(string name) : base(name)
+        {
+        }
+
+        public GreenTxtFileManager(string name, string folderPath, string fileName, string fileExtension = "json") : base(name, folderPath, fileName, fileExtension)
+        {
+        }
+        public override void EditFile(string newText)
+        {
+            var obj = Deserialize<Lab9.Green.Green>();
+            obj.ChangeText(newText);
+            Serialize(obj);
+        }
+
+        public override void ChangeFileExtension(string newFileExtension)
+        {
+            ChangeFileFormat("txt");
+        }
+
+        public override void Serialize<T>(T obj)
+        {
+            ChangeFileFormat("txt");
+            string content = "Type;;;" + obj.GetType().AssemblyQualifiedName + Environment.NewLine;
+            Type currentType = obj.GetType();
+            while (currentType != null && currentType != typeof(object))
+            {
+                var fields = currentType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    var field = fields[i];
+                    var value = field.GetValue(obj);
+                    if (value is string valStr)
+                    {
+                        string cleanValue = valStr.Replace("\n", "[-n-]");
+                        content += field.Name + ";;;" + cleanValue + Environment.NewLine;
+                    }
+                }
+                currentType = currentType.BaseType;
+            }
+            File.WriteAllText(FullPath, content);
+        }
+
+        public override T Deserialize<T>()
+        {
+            string[] lines = File.ReadAllLines(FullPath);
+            string typeStr = lines[0].Split(";;;")[1];
+            Type type1 = Type.GetType(typeStr);
+
+            var data = new Dictionary<string, string>();
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var parts = lines[i].Split(";;;");
+                data[parts[0]] = parts[1].Replace("[-n-]", "\n");
+            }
+
+            var constructor = type1.GetConstructors()[0];
+            var parameters = constructor.GetParameters();
+            object[] args = new object[parameters.Length];
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                string pName = parameters[i].Name;
+
+                if (data.TryGetValue("_" + pName, out string val))
+                {
+                    args[i] = val;
+                }
+
+                else if (data.TryGetValue("_input", out string inputVal))
+                {
+                    args[i] = inputVal;
+                }
+            }
+
+            T res = (T)constructor.Invoke(args);
+
+            if (data.TryGetValue("_input", out string finalInput))
+            {
+                res.ChangeText(finalInput);
+            }
+
+            return res;
+        }
+    }
+}
