eae09dd25d7ab88aa6387bc4a64fa51e944de760
Alohamod <airat.grafon@gmail.com>
2026-05-10 21:37:03 +0300

    Add files via upload

--- Lab10Test/Purple/PurpleManagerTest.cs
+++ Lab10Test/Purple/PurpleManagerTest.cs
@@ -1,243 +1,127 @@
-﻿//using Lab10.Purple;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System.Text.Json;
+﻿using System.Xml.Serialization;
+using Lab9.Purple;
 
-//namespace Lab10Test.Purple
-//{
-//    [TestClass]
-//    public sealed class PurpleManagerTest
-//    {
-//        private Lab9.Purple.Purple[] _tasks;
-//        private string[] _input;
-//        private (string pair, char code)[][] _codes;
+namespace Lab10.Purple;
 
-//        [TestInitialize]
-//        public void LoadData()
-//        {
-//            var folder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
-//            var file = Path.Combine(folder, "Lab10Test", "Purple", "data.json");
+public class PurpleXmlFileManager<T> : PurpleFileManager<T> where T : Lab9.Purple.Purple
+{
+    public PurpleXmlFileManager(string name) : base(name)
+    {
+    }
 
-//            var json = JsonSerializer.Deserialize<JsonElement>(File.ReadAllText(file));
+    public PurpleXmlFileManager(string name, string folderName, string fileName, string fileExtension = "txt") : base(name, folderName, fileName, fileExtension)
+    {
+    }
 
-//            _input = json.GetProperty("Task4").GetProperty("input").Deserialize<string[]>();
+    public override T Deserialize()
+    {
+        if (FullPath == null || !File.Exists(FullPath)) return null;
 
-//            var codesJson = json.GetProperty("Task4").GetProperty("codes");
-//            var codesArray = codesJson.Deserialize<CodePair[][]>();
+        var serializer = new XmlSerializer(typeof(DTOPurple));
+        DTOPurple dto;
 
-//            _codes = new (string pair, char code)[codesArray.Length][];
+        using (StreamReader reader = new StreamReader(FullPath))
+        {
+            dto = (DTOPurple)serializer.Deserialize(reader);
+        }
 
-//            for (int i = 0; i < codesArray.Length; i++)
-//            {
-//                _codes[i] = new (string, char)[codesArray[i].Length];
-//                for (int j = 0; j < codesArray[i].Length; j++)
-//                {
-//                    _codes[i][j] = (codesArray[i][j].pair, codesArray[i][j].code[0]);
-//                }
-//            }
-//        }
+        (string, char)[] codes = null;
+        if (dto.CodePairs != null)
+        {
+            codes = new (string, char)[dto.CodePairs.Length];
+            for (int i = 0; i < dto.CodePairs.Length; i++)
+            {
+                codes[i] = (dto.CodePairs[i].Substring(0, dto.CodePairs[i].Length - 2), dto.CodePairs[i][dto.CodePairs[i].Length - 1]);
+                Console.WriteLine(codes[i]);
+            }
+        }
+        Lab9.Purple.Purple result;
+        if (dto.TypeName == "Task1")
+        {
+            result = new Task1(dto.Input);
+        }
+        else if (dto.TypeName == "Task2")
+        {
+            result = new Task2(dto.Input);
+        }
+        else if (dto.TypeName == "Task3")
+        {
+            result = new Task3(dto.Input);
+        }
+        else if (dto.TypeName == "Task4")
+        {
+            result = new Task4(dto.Input, codes);
+        }
+        else
+        {
+            result = null;
+        }
 
-//        private void Init(int i)
-//        {
-//            var taskCodes = _codes[i];
+        if (result == null) return null;
+        result.Review();
+        return (T)result;
+    }
 
-//            _tasks = new Lab9.Purple.Purple[]
-//            {
-//                 new Lab9.Purple.Task1(_input[i]),
-//                 new Lab9.Purple.Task2(_input[i]),
-//                 new Lab9.Purple.Task3(_input[i]),
-//                 new Lab9.Purple.Task4(_input[i], taskCodes)
-//            };
+    public override void Serialize(T obj)
+    {
+        if (obj == null || FullPath == null) return;
 
-//            foreach (var t in _tasks)
-//                t.Review();
-//        }
+        var serializer = new XmlSerializer(typeof(DTOPurple));
+        using StreamWriter writer = new StreamWriter(FullPath);
+        serializer.Serialize(writer, new DTOPurple(obj));
+    }
 
-//        [TestMethod]
-//        public void Test_00_OOP()
-//        {
-//            var type = typeof(Lab10.Purple.Purple<Lab9.Purple.Purple>);
+    public override void EditFile(string input)
+    {
+        T content = Deserialize();
+        content.ChangeText(input);
+        Serialize(content);
+    }
 
-//            Assert.IsTrue(type.IsClass, "Purple<T> must be class");
+    public override void ChangeFileExtension(string extension)
+    {
+        T content = Deserialize();
+        if (extension == "txt") ChangeFileFormat("txt");
+        Serialize(content);
+    }
 
-//            Assert.IsNotNull(type.GetProperty("Manager"));
-//            Assert.IsNotNull(type.GetProperty("Tasks"));
+    public class DTOPurple
+    {
+        public string TypeName { get; set; }
+        public string Input { get; set; }
+        public string[] CodePairs { get; set; }
 
-//            Assert.IsNotNull(type.GetMethod("Add", new[] { typeof(Lab9.Purple.Purple) }));
-//            Assert.IsNotNull(type.GetMethod("Add", new[] { typeof(Lab9.Purple.Purple[]) }));
-//            Assert.IsNotNull(type.GetMethod("Remove", new[] { typeof(Lab9.Purple.Purple) }));
-//            Assert.IsNotNull(type.GetMethod("Clear"));
-//            Assert.IsNotNull(type.GetMethod("SaveTasks"));
-//            Assert.IsNotNull(type.GetMethod("LoadTasks"));
-//            Assert.IsNotNull(type.GetMethod("ChangeManager"));
-//        }
+        public DTOPurple() { }
 
-//        [TestMethod]
-//        public void Test_01_Add_Remove_AllTasks()
-//        {
-//            for (int i = 0; i < _input.Length; i++)
-//            {
-//                Init(i);
+        public DTOPurple(Lab9.Purple.Purple obj)
+        {
+            TypeName = obj.GetType().Name;
+            Input = obj.Input;
 
-//                var w = new Lab10.Purple.Purple<Lab9.Purple.Purple>();
+            (string, char)[] codes;
+            if (obj is Task3 task3)
+            {
+                codes = task3.Codes;
+            }
+            else
+            {
+                codes = null;
+            }
+            if (obj is Task4 task4)
+            {
+                codes = task4.Codes;
+            }
+            else
+            {
+                codes = null;
+            }
 
-//                w.Add(_tasks);
-//                Assert.AreEqual(_tasks.Length, w.Tasks.Length, $"Add failed test {i}");
-
-//                w.Remove(_tasks[0]);
-//                Assert.IsFalse(w.Tasks.Contains(_tasks[0]), $"Remove failed test {i}");
-//            }
-//        }
-
-//        [TestMethod]
-//        public void Test_02_Save_Load_Txt()
-//        {
-//            for (int i = 0; i < _input.Length; i++)
-//            {
-//                Init(i);
-
-//                var manager = new PurpleTxtFileManager<Lab9.Purple.Purple>("txt");
-//                var folder = Path.Combine(Path.GetTempPath(), $"PurpleTxt_{i}");
-//                Directory.CreateDirectory(folder);
-
-//                manager.SelectFolder(folder);
-
-//                var w = new Lab10.Purple.Purple<Lab9.Purple.Purple>(manager, _tasks);
-
-//                w.SaveTasks();
-//                w.LoadTasks();
-
-//                for (int j = 0; j < _tasks.Length; j++)
-//                {
-//                    Assert.AreEqual(_tasks[j].Input, w.Tasks[j].Input,
-//                        $"TXT load mismatch test {i} task {j}");
-//                }
-
-//                Directory.Delete(folder, true);
-//            }
-//        }
-
-//        [TestMethod]
-//        public void Test_03_Save_Load_Json()
-//        {
-//            for (int i = 0; i < _input.Length; i++)
-//            {
-//                Init(i);
-
-//                var manager = new PurpleJsonFileManager<Lab9.Purple.Purple>("json");
-//                var folder = Path.Combine(Path.GetTempPath(), $"PurpleJson_{i}");
-//                Directory.CreateDirectory(folder);
-
-//                manager.SelectFolder(folder);
-
-//                var w = new Lab10.Purple.Purple<Lab9.Purple.Purple>(manager, _tasks);
-
-//                w.SaveTasks();
-//                w.LoadTasks();
-
-//                for (int j = 0; j < _tasks.Length; j++)
-//                {
-//                    Assert.AreEqual(_tasks[j].Input, w.Tasks[j].Input,
-//                        $"JSON load mismatch test {i} task {j}");
-//                }
-
-//                Directory.Delete(folder, true);
-//            }
-//        }
-
-//        [TestMethod]
-//        public void Test_04_Save_Load_Xml()
-//        {
-//            for (int i = 0; i < _input.Length; i++)
-//            {
-//                Init(i);
-
-//                var manager = new PurpleXmlFileManager<Lab9.Purple.Purple>("xml");
-//                var folder = Path.Combine(Path.GetTempPath(), $"PurpleXml_{i}");
-//                Directory.CreateDirectory(folder);
-
-//                manager.SelectFolder(folder);
-
-//                var w = new Lab10.Purple.Purple<Lab9.Purple.Purple>(manager, _tasks);
-
-//                w.SaveTasks();
-//                w.LoadTasks();
-
-//                for (int j = 0; j < _tasks.Length; j++)
-//                {
-//                    Assert.AreEqual(_tasks[j].Input, w.Tasks[j].Input,
-//                        $"XML load mismatch test {i} task {j}");
-//                }
-
-//                Directory.Delete(folder, true);
-//            }
-//        }
-
-//        [TestMethod]
-//        public void Test_05_ChangeManager_And_Format()
-//        {
-//            for (int i = 0; i < _input.Length; i++)
-//            {
-//                Init(i);
-
-//                var txtManager = new PurpleTxtFileManager<Lab9.Purple.Purple>("txt");
-//                var jsonManager = new PurpleJsonFileManager<Lab9.Purple.Purple>("json");
-//                var xmlManager = new PurpleXmlFileManager<Lab9.Purple.Purple>("xml");
-
-//                var folder = Path.Combine(Path.GetTempPath(), $"PurpleMix_{i}");
-//                Directory.CreateDirectory(folder);
-
-//                txtManager.SelectFolder(folder);
-
-//                var w = new Lab10.Purple.Purple<Lab9.Purple.Purple>(txtManager, _tasks);
-
-//                w.SaveTasks();
-
-//                // Меняем с TXT на JSON
-//                w.ChangeManager(jsonManager);
-//                w.LoadTasks();
-
-//                int loadedCount = w.Tasks.Count(t => t != null);
-//                Assert.IsTrue(loadedCount < _tasks.Length,
-//                    $"ChangeManager from TXT to JSON should break loading in test {i}");
-
-//                // Меняем с JSON на XML
-//                w.ChangeManager(xmlManager);
-//                w.LoadTasks();
-
-//                loadedCount = w.Tasks.Count(t => t != null);
-//                Assert.IsTrue(loadedCount < _tasks.Length,
-//                    $"ChangeManager from JSON to XML should break loading in test {i}");
-
-//                Directory.Delete(folder, true);
-//            }
-//        }
-
-//        [TestMethod]
-//        public void Test_06_Clear_All()
-//        {
-//            for (int i = 0; i < _input.Length; i++)
-//            {
-//                Init(i);
-
-//                var manager = new PurpleTxtFileManager<Lab9.Purple.Purple>("clear");
-//                var folder = Path.Combine(Path.GetTempPath(), $"PurpleClear_{i}");
-//                Directory.CreateDirectory(folder);
-
-//                manager.SelectFolder(folder);
-
-//                var w = new Lab10.Purple.Purple<Lab9.Purple.Purple>(manager, _tasks);
-
-//                w.Clear();
-
-//                Assert.AreEqual(0, w.Tasks.Length, $"Clear failed test {i}");
-//                Assert.IsFalse(Directory.Exists(folder), $"Folder not deleted test {i}");
-//            }
-//        }
-
-//        private class CodePair
-//        {
-//            public string pair { get; set; } = "";
-//            public string code { get; set; } = "";
-//        }
-//    }
-//}
+            if (codes != null)
+            {
+                CodePairs = new string[codes.Length];
+                for (int i = 0; i < codes.Length; i++)
+                    CodePairs[i] = codes[i].Item1 + " " + codes[i].Item2;
+            }
+        }
+    }
+}
