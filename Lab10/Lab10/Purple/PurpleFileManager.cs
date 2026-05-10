39d2cba632b9faecf96553b5b9bbccd8981c36ab
nvolokitina7 <nvolokitina7@gmail.com>
2026-05-10 23:51:36 +0300

    Update PurpleFileManager.cs

--- Lab10/Lab10/Purple/PurpleFileManager.cs
+++ Lab10/Lab10/Purple/PurpleFileManager.cs
@@ -1,16 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lab10.Purple;
 
 namespace Lab10.Purple
 {
-    public abstract class PurpleFileManager<T> : MyFileManager, IPurpleSerializer<T> where T : Lab9.Purple.Purple
+    public abstract class PurpleFileManager<T> : MyFileManager, ISerializer<T> where T : Lab9.Purple.Purple
     {
-        protected PurpleFileManager(string name) : base(name) { }
-        // Доступно только внутри этого класса и его наследников; вызов конструктора родителя.
-        protected PurpleFileManager(string name, string folderPath, string filename, string fileExtension = "") : base(name, folderPath, filename, fileExtension) { }
+        public PurpleFileManager(string name) : base(name) { }
+        public PurpleFileManager(string name, string folderPath, string filename, string fileExtension = "") : base(name, folderPath, filename, fileExtension) { }
         public override void EditFile(string a)
         {
             if (!File.Exists(FullPath)) return;
@@ -20,7 +20,7 @@ namespace Lab10.Purple
         {
             if (!File.Exists(FullPath)) return;
             string Path1=FullPath; // где файл лежал ДО изменения расширения
-            ChangeFileFormat(e); // Меняем расширение в памяти
+            base.ChangeFileFormat(e); // Меняем расширение в памяти
             string Path2=FullPath; // ПОСЛЕ изменения
             File.Move(Path1, Path2); //Перемещает/переименовывает файл на диске (физ)
         }
