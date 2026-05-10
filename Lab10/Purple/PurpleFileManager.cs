c9f4e7a896e0029de9e3b6555e77ea0a6d00ab4c
vinograd <vinograd1020gm@gmail.com>
2026-05-10 18:40:41 +0300

    Commit

---
+++ Lab10/Purple/PurpleFileManager.cs
@@ -0,0 +1,23 @@
+namespace Lab10.Purple;
+
+public abstract class PurpleFileManager<T> : MyFileManager, ISerializer<T> where T : Lab9.Purple.Purple
+{
+    protected PurpleFileManager(string name) : base(name) { }
+
+    protected PurpleFileManager(string name, string folderName, string fileName, string fileExtension = "txt") : base(name, folderName, fileName, fileExtension) { }
+
+    public abstract T Deserialize();
+
+    public abstract void Serialize(T obj);
+
+    public override void EditFile(string input)
+    {
+        base.EditFile(input);
+    }
+    
+    public override void ChangeFileExtension(string extension)
+    {
+        base.ChangeFileExtension(extension);
+    }
+    
+}
