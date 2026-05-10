3e9375478a2e28c778b1ee1a11fd9e53754a1a53
Georgy <georgy_mann@mail.ru>
2026-05-10 16:39:58 +0300

    move lab9 sln

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Lab10
+﻿using Lab10.Blue;
+namespace Lab10
 {
     public class Program
     {
         public static void Main()
         {
+            var manager = new BlueJsonFileManager<Lab9.Blue.Blue>("test", "TestJson", "task", "json");
+
+            var task = new Lab9.Blue.Task2("самолет играет важную роль", "иг");
+            task.Review();
+
+            manager.Serialize(task);
+
+            var result = manager.Deserialize();
+
+            Console.WriteLine(File.Exists(manager.FullPath));
+            Console.WriteLine(task.ToString());
+            Console.WriteLine(result.ToString());
+            
+            Console.WriteLine(Path.GetFullPath(manager.FullPath));
+
+
+            string test = "привет, как дела, спишь?";
+            string res = test.Substring("привет, ".Length);
+            Console.WriteLine(res);
         }
     }
 }
