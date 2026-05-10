2d60f734edededb64550611d41b13635ed4c7d9a
OleshkaDa <o-liska@bk.ru>
2026-05-10 21:21:11 +0300

    Add files via upload

---
+++ Lab9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace Lab9
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] kulebaka = new string[2] { "интеграл", "гармония" };
+            //Console.WriteLine(CultureInfo.CurrentCulture.Name);
+            Console.WriteLine(kulebaka[1]);
+        }
+    }
+}
