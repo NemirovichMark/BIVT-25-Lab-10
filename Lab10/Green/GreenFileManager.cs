f66e0eba2608ea9f939ad7c47bc4b258d4a5f8b2
Lukashev Roman <74509534+ApparitionVViz@users.noreply.github.com>
2026-05-10 23:53:53 +0300

    Add files via upload

---
+++ Lab10/Green/GreenFileManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab9.Green;
+
+namespace Lab10.Green
+{
+    public abstract class GreenFileManager : MyFileManager, ISerializer
+    {
+        protected GreenFileManager(string name) : base(name)
+        {
+        }
+
+        protected GreenFileManager(string name, string folderPath, string fileName, string fileExtension = "txt") : base(name, folderPath, fileName, fileExtension)
+        {
+        }
+
+        public override void EditFile(string content)
+        {
+            base.EditFile(content);
+        }
+
+        public override void ChangeFileExtension(string newExtension)
+        {
+            base.ChangeFileExtension(newExtension);
+        }
+
+        public abstract T Deserialize<T>() where T : Lab9.Green.Green;
+
+        public abstract void Serialize<T>(T obj) where T : Lab9.Green.Green;
+    }
+}
