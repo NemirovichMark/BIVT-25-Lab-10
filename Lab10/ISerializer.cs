44916e62bf48a565c4bd38f25fda0b0e7d3e4b92
OleshkaDa <o-liska@bk.ru>
2026-05-10 21:25:12 +0300

    Add files via upload

---
+++ Lab10/ISerializer.cs
@@ -0,0 +1,18 @@
+﻿using Lab9.Green;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    public interface ISerializer
+    {
+        public abstract T Deserialize<T>()
+    where T : Lab9.Green.Green;
+
+        public abstract void Serialize<T>(T obj)
+            where T : Lab9.Green.Green;
+    }
+}
