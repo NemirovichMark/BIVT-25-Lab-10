c494e9224fb01fd58acf3058f6d9c92bf32ff1e9
agrowow <ivan.pechersky07@icloud.com>
2026-05-10 13:19:30 +0300

    ISerializer.cs

--- Lab10/ISerializer.cs
+++ Lab10/ISerializer.cs
@@ -1,9 +1,8 @@
 namespace Lab10
 {
-    public interface ISerializer<T> where T : Lab9.Blue.Blue
+    public interface ISerializer<T> where T : Lab10.Blue.Blue
     {
         void Serialize(T obj);
-
         T Deserialize();
     }
 }
