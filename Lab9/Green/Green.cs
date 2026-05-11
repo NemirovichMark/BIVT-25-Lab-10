451ea2cd56106625d78dcc90be8734c3edb2edc7
AnnaKomova <akomova456@gmail.com>
2026-05-11 21:33:30 +0300

    Add files via upload

---
+++ Lab9/Green/Green.cs
@@ -0,0 +1,24 @@
+namespace Lab9.Green
+{
+    public abstract class Green
+    {
+        private string _input;
+        public string Input => _input;
+
+        protected Green(string input)
+        {
+            _input = input;
+        }
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+    }
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> Add files via upload
