da014e1a9cbfe561ba91dac32049ce655347268a
ddarina <126961692+mitarayushka@users.noreply.github.com>
2026-05-12 00:01:03 +0300

    Update Purple Task1.cs

--- Lab9/Purple/Task1.cs
+++ Lab9/Purple/Task1.cs
@@ -1,39 +1,39 @@
-namespace Lab9.Purple;
+// namespace Lab9.Purple;
 
-public class Task1 : Purple
-{
-    private string _output;
-    public string Output => _output;
+// public class Task1 : Purple
+// {
+//     private string _output;
+//     public string Output => _output;
 
-    public Task1(string text) : base(text)
-    { 
-        _output = "";
+//     public Task1(string text) : base(text)
+//     { 
+//         _output = "";
         
-    }
+//     }
 
-    public override void Review()
-    {
-        char[] chars = {'.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/', ' '};
-        int n = _input.Length;
-        string word = "";
-        string revWord;
-        bool rev = true;
+//     public override void Review()
+//     {
+//         char[] chars = {'.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/', ' '};
+//         int n = _input.Length;
+//         string word = "";
+//         string revWord;
+//         bool rev = true;
 
 
-        for (int i = 0; i < n; i++)
-        {
-            if (chars.Contains(_input[i]))
-            {
-                if (char.IsDigit(_input[i - 1]) && i + 1 < n && char.IsDigit(_input[i + 1]) && _input[i] == ',')
-                {
-                    word += _input[i];
-                }
-                else
-                {
-                    if (rev == false)
-                    {
-                        _output += word + _input[i];
-                        word = "";
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (chars.Contains(_input[i]))
+//             {
+//                 if (char.IsDigit(_input[i - 1]) && i + 1 < n && char.IsDigit(_input[i + 1]) && _input[i] == ',')
+//                 {
+//                     word += _input[i];
+//                 }
+//                 else
+//                 {
+//                     if (rev == false)
+//                     {
+//                         _output += word + _input[i];
+//                         word = "";
                         rev = true;
                     }
                     else
