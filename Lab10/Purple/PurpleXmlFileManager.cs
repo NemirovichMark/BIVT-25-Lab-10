f5be116051b4691ab7a0ac4289f09ff7bedbc22a
Kantsler <igorkantsur07@gmail.com>
2026-05-11 21:28:34 +0300

    Refactor PurpleXmlFileManager for improved functionality

--- Lab10/Purple/PurpleXmlFileManager.cs
+++ Lab10/Purple/PurpleXmlFileManager.cs
@@ -1,111 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Lab9.Purple;
 
-namespace Lab10.Purple;
-
-public class PurpleXmlFileManager<T> : PurpleFileManager<T>
-    where T : Lab9.Purple.Purple
+namespace Lab10.Purple
 {
-    public PurpleXmlFileManager(string name) : base(name) { }
-
-    public PurpleXmlFileManager(string name, string folderPath, string fileName, string fileExtension = "xml")
-        : base(name, folderPath, fileName, fileExtension) { }
-
-    public override void EditFile(string text)
+    public class PurpleXmlFileManager<T> : PurpleFileManager<T> where T : Lab9.Purple.Purple
     {
-        if (string.IsNullOrWhiteSpace(text) || !File.Exists(FullPath))
-            return;
-
-        T obj = Deserialize();
-        if (obj == null)
-            return;
-
-        obj.ChangeText(text);
-        Serialize(obj);
-    }
-
-    public override void ChangeFileExtension(string extension)
-    {
-        if (string.IsNullOrWhiteSpace(extension) || extension != "xml")
-            return;
-
-        if (!File.Exists(FullPath))
+        public PurpleXmlFileManager(string name) : base(name)
         {
-            ChangeFileFormat("xml");
-            return;
         }
 
-        T obj = Deserialize();
-        ChangeFileFormat("xml");
-        if (obj != null)
+        public PurpleXmlFileManager(string name, string folderName, string fileName, string fileExtension = "txt") : base(name, folderName, fileName, fileExtension)
         {
-            Serialize(obj);
         }
-    }
 
-    public override void Serialize(T obj)
-    {
-        if (obj == null)
-            return;
-
-        if (!Directory.Exists(FolderPath))
-            Directory.CreateDirectory(FolderPath);
-
-        try
+        public override T Deserialize()
         {
-            var table = GetTask4Table(obj);
-            DTOItem[] items = table.Length == 0
-                ? Array.Empty<DTOItem>()
-                : table.Select(item => new DTOItem(item.Item1, item.Item2)).ToArray();
+            if (FullPath == null || !File.Exists(FullPath)) return null;
 
-            DTOPurple dto = new DTOPurple(obj.GetType().Name, obj.Input, items);
-            XmlSerializer serializer = new XmlSerializer(typeof(DTOPurple));
-
-            using (StreamWriter sw = new StreamWriter(FullPath))
+            var serializer = new XmlSerializer(typeof(DTOPurple));
+            DTOPurple dto;
+            using (StreamReader reader = new StreamReader(FullPath))
             {
-                serializer.Serialize(sw, dto);
+                dto = (DTOPurple)serializer.Deserialize(reader);
             }
-        }
-        catch
-        {
-        }
-    }
-
-    public override T Deserialize()
-    {
-        if (!File.Exists(FullPath))
-            return null;
-
-        try
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(DTOPurple));
-            DTOPurple dto = null;
-
-            using (StreamReader sr = new StreamReader(FullPath))
+            (string, char)[] codes = null;
+            if (dto.CodePairs != null)
             {
-                object deserialized = serializer.Deserialize(sr);
-                dto = deserialized as DTOPurple;
+                codes = new (string, char)[dto.CodePairs.Length];
+                for (int i = 0; i < dto.CodePairs.Length; i++)
+                {
+                    codes[i] = (dto.CodePairs[i].Substring(0, dto.CodePairs[i].Length - 2), dto.CodePairs[i][dto.CodePairs[i].Length - 1]);
+                }
+            }
+            Lab9.Purple.Purple result;
+            if (dto.TypeName == "Task1")
+            {
+                result = new Task1(dto.Input);
+            } else if (dto.TypeName == "Task2")
+            {
+                result = new Task2(dto.Input);
+            }else if (dto.TypeName == "Task3")
+            {
+                result = new Task3(dto.Input);
+            } else if (dto.TypeName == "Task4")
+            {
+                result = new Task4(dto.Input, codes);
+            } else
+            {
+                result = null;
             }
 
-            if (dto == null || string.IsNullOrEmpty(dto.TypeName) || string.IsNullOrEmpty(dto.Input))
-                return null;
+            if (result == null) return null;
+            result.Review();
+            return (T)result;
+        }
 
-            var codes = dto.Items == null || dto.Items.Length == 0
-                ? Array.Empty<(string, char)>()
-                : dto.Items.Select(item => (item.Key, item.Value)).ToArray();
+        public override void Serialize(T obj)
+        {
+            if (obj == null || FullPath == null) return;
 
-            var obj = CreateTask(dto.TypeName, dto.Input, codes);
-            if (obj != null)
+            var serializer = new XmlSerializer(typeof(DTOPurple));
+            using StreamWriter writer = new StreamWriter(FullPath);
+            serializer.Serialize(writer, new DTOPurple(obj));
+        }
+
+        public override void EditFile(string input)
+        {
+            T content = Deserialize();
+            content.ChangeText(input);
+            Serialize(content);
+        }
+
+        public override void ChangeFileExtension(string extension)
+        {
+            T content = Deserialize();
+            if (extension == "txt") ChangeFileFormat("txt");
+            Serialize(content);
+        }
+
+        public class DTOPurple
+        {
+            public string TypeName { get; set; }
+            public string Input { get; set; }
+            public string[] CodePairs { get; set; }
+            
+            public DTOPurple() { }
+
+            public DTOPurple(Lab9.Purple.Purple obj)
             {
-                obj.Review();
-                return (T)obj;
+                TypeName = obj.GetType().Name;
+                Input = obj.Input;
+
+                (string, char)[] codes;
+                if (obj is Task3 task3)
+                {
+                    codes = task3.Codes;
+                }
+                else
+                {
+                    codes = null;
+                }
+                if (obj is Task4 task4)
+                {
+                    codes = task4.Codes;
+                }
+                else
+                {
+                    codes = null;
+                }
+
+                if (codes != null)
+                {
+                    CodePairs = new string[codes.Length];
+                    for (int i = 0; i < codes.Length; i++)
+                        CodePairs[i] = codes[i].Item1 + " " + codes[i].Item2;
+                }
             }
         }
-        catch
-        {
-            return null;
-        }
-
-        return null;
     }
 }
