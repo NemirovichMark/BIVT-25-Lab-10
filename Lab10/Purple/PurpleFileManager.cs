d29cfb7a4a30bdc34717189ce59385b4b04476a4
Kantsler <igorkantsur07@gmail.com>
2026-05-11 21:27:16 +0300

    Update PurpleFileManager.cs

--- Lab10/Purple/PurpleFileManager.cs
+++ Lab10/Purple/PurpleFileManager.cs
@@ -1,61 +1,33 @@
-using System.Reflection;
-using Lab9.Purple;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace Lab10.Purple;
-
-public abstract class PurpleFileManager<T> : MyFileManager, ISerializer<T> where T : Lab9.Purple.Purple
+namespace Lab10.Purple
 {
-    public PurpleFileManager(string name) : base(name) { }
-
-    public PurpleFileManager(string name, string folderpath, string filename, string fileextension = "txt")
-        : base(name, folderpath, filename, fileextension) { }
-
-    protected static T CreateTask(string kind, string input, (string, char)[] codes = null)
+    public abstract class PurpleFileManager<T> : MyFileManager, ISerializer<T> where T : Lab9.Purple.Purple
     {
-        codes ??= Array.Empty<(string, char)>();
-
-        return kind switch
+        protected PurpleFileManager(string name) : base(name)
         {
-            nameof(Task1) => new Task1(input) as T,
-            nameof(Task2) => new Task2(input) as T,
-            nameof(Task3) => new Task3(input) as T,
-            nameof(Task4) => new Task4(input, codes) as T,
-            _ => null
-        };
-    }
-
-    protected static (string, char)[] GetTask4Table(Lab9.Purple.Purple task)
-    {
-        if (task is not Task4 task4)
-        {
-            return Array.Empty<(string, char)>();
         }
 
-        var field = typeof(Task4).GetField("_table", BindingFlags.NonPublic | BindingFlags.Instance);
-        if (field?.GetValue(task4) is (string, char)[] table && table != null)
+        protected PurpleFileManager(string name, string folderName, string fileName, string fileExtension = "txt") : base(name, folderName, fileName, fileExtension)
         {
-            return table;
         }
 
-        return Array.Empty<(string, char)>();
+        public abstract T Deserialize();
+
+        public abstract void Serialize(T obj);
+
+        public override void EditFile(string input)
+        {
+            base.EditFile(input);
+        }
+        public override void ChangeFileExtension(string extension)
+        {
+            base.ChangeFileExtension(extension);
+        }
     }
-
-    public override void EditFile(string text)
-    {
-        if (string.IsNullOrWhiteSpace(text) || !File.Exists(FullPath))
-            return;
-
-        base.EditFile(text);
-    }
-
-    public override void ChangeFileExtension(string extension)
-    {
-        if (string.IsNullOrWhiteSpace(extension) || !File.Exists(FullPath))
-            return;
-
-        base.ChangeFileExtension(extension);
-    }
-
-    public abstract void Serialize(T obj);
-    public abstract T Deserialize();
 }
