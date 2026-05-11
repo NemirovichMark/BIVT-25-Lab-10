6c15017f038716cfa8f281143abb34da11b3be49
Kantsler <igorkantsur07@gmail.com>
2026-05-11 21:33:21 +0300

    Add files via upload

---
+++ Lab10/IFileLifeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    public interface IFileLifeController
+    {
+        void CreateFile();
+        void DeleteFile();
+        void EditFile(string fileName);
+        void ChangeFileExtension(string extension);
+    }
+}
