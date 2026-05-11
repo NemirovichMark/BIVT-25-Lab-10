a20a005cc81b46e4cce0e82f9da96b59b7f5d099
ddarina <126961692+mitarayushka@users.noreply.github.com>
2026-05-12 00:33:45 +0300

    Add files via upload

---
+++ Lab10/White/IWhiteSerializer.cs
@@ -0,0 +1,11 @@
+using Lab9.White;
+using Lab10;
+
+namespace Lab10
+{
+    public interface IWhiteSerializer 
+    {
+        Lab9.White.White Deserialize();
+        void Serialize(Lab9.White.White obj);
+    }
+}
---
+++ Lab10/White/WhiteTxtFileManager.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using Lab10;
+using Lab9.White;
+
+namespace Lab10.White 
+{
+    public class WhiteTxtFileManager : WhiteFileManager 
+    {
+        public WhiteTxtFileManager(string name) : base(name) {}
+
+        public WhiteTxtFileManager(string name, string folder, string file) : base(name, folder, file, ".txt") { }
+
+        public override void Serialize(Lab9.White.White obj) {
+            if (obj == null || string.IsNullOrEmpty(FullPath)) return;
+            File.WriteAllText(FullPath, obj.ToString()); 
+        }
+
+        public override Lab9.White.White Deserialize() 
+        {
+            if (!File.Exists(FullPath)) return null;
+            string content = File.ReadAllText(FullPath);
+            return new Lab9.White.Task1(content);
+        }
+    }
+}
