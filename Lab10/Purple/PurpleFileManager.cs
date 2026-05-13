480880c98cb1898ae0b01af38337b2db25ab30bc
liza-127 <lizadaystar12737@gmail.com>
2026-05-14 00:51:06 +0300

    Add files via upload

---
+++ Lab10/Purple/PurpleFileManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lab10.Purple;
+
+public abstract class PurpleFileManager<T> : MyFileManager, ISerializer<T> where T : Lab9.Purple.Purple
+{
+    public PurpleFileManager(string name) : base(name) { }
+    public PurpleFileManager(string name, string folderpath, string filename, string fileextension = "txt") : base(name, folderpath, filename, fileextension) { }
+
+    public override void EditFile(string file)
+    {
+        if (!File.Exists(FullPath)) return;
+        base.EditFile(file);
+    }
+    public virtual void ChangeFileExtension(string extension)
+    {
+        if (!File.Exists(FullPath)) return;
+        base.ChangeFileExtension(extension);
+    }
+
+    public abstract void Serialize(T obj);
+    public abstract T Deserialize();
+}
+
