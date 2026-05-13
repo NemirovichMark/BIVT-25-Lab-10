3ce229f0d13e10fab559148fa152371e03f90341
liza-127 <lizadaystar12737@gmail.com>
2026-05-14 00:51:49 +0300

    Add files via upload

---
+++ Lab10/IFileLifeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    public interface IFileLifeController
+    {
+        // CreateFile, DeleteFile
+        void CreateFile();
+        void DeleteFile();
+        //  EditFile и ChangeFileExtension
+        void EditFile(string file);
+        void ChangeFileExtension(string file);
+    }
+}
