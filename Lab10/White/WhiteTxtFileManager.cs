7dfb3574e6eb416f50cf02a56eeb067c9ef97d40
ddarina <126961692+mitarayushka@users.noreply.github.com>
2026-05-15 13:23:35 +0300

    Update WhiteTxtFileManager.cs

--- Lab10/White/WhiteTxtFileManager.cs
+++ Lab10/White/WhiteTxtFileManager.cs
@@ -10,12 +10,12 @@ namespace Lab10.White
 
         public WhiteTxtFileManager(string name, string folder, string file) : base(name, folder, file, ".txt") { }
 
-        public override void Serialize(Lab9.White.White obj) {
+        public override void Serialize(Lab10.White.White obj) {
             if (obj == null || string.IsNullOrEmpty(FullPath)) return;
             File.WriteAllText(FullPath, obj.ToString()); 
         }
 
-        public override Lab9.White.White Deserialize() 
+        public override Lab10.White.White Deserialize() 
         {
             if (!File.Exists(FullPath)) return null;
             string content = File.ReadAllText(FullPath);
