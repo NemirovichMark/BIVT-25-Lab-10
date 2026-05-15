690ad644e667088565dfc183fd396f005182cf9d
ddarina <126961692+mitarayushka@users.noreply.github.com>
2026-05-15 19:29:09 +0300

    bug fix

--- Lab10/White/WhiteFileManager.cs
+++ Lab10/White/WhiteFileManager.cs
@@ -1,24 +1,30 @@
 using System.IO;
-using Lab10;
-using Lab10.White;
 
-namespace Lab10.White 
+namespace Lab10.White
 {
     public abstract class WhiteFileManager : MyFileManager, IWhiteSerializer
     {
         public WhiteFileManager(string name) : base(name) { }
-        public WhiteFileManager(string name, string folder, string fileName, string ext) 
+
+        public WhiteFileManager(string name, string folder, string fileName, string ext = "txt")
             : base(name, folder, fileName, ext) { }
 
         public abstract void Serialize(Lab9.White.White obj);
         public abstract Lab9.White.White Deserialize();
-    
-        public override void EditFile(string content) => File.WriteAllText(FullPath, content);
-        public override void ChangeFileExtension(string newExt) 
+
+        public override void EditFile(string content)
+        {
+            if (!string.IsNullOrEmpty(FullPath)) File.WriteAllText(FullPath, content);
+        }
+
+        public override void ChangeFileExtension(string extension)
         {
             string oldPath = FullPath;
-            base.ChangeFileFormat(newExt);
-            if (File.Exists(oldPath)) File.Move(oldPath, FullPath, true);
+            ChangeFileFormat(extension);
+            if (File.Exists(oldPath))
+            {
+                File.Move(oldPath, FullPath, true);
+            }
         }
     }
 }
