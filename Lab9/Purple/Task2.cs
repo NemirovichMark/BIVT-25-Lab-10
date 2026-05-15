084315d9f6c84bbc28ef07626a539720a2f97cd6
ddarina <126961692+mitarayushka@users.noreply.github.com>
2026-05-15 13:05:55 +0300

    Update Task2.cs

--- Lab9/Purple/Task2.cs
+++ Lab9/Purple/Task2.cs
@@ -34,69 +34,69 @@
 //             _output[^1] = words[0];
 //             string substring = _output[^1];
 
-            for (int i = 1; i < words.Length; i++)
-            {
-                substring += " " + words[i];
-                if (substring.Length <= 50)
-                {
-                    _output[^1] = substring;
-                }
-                else
-                {
-                    Array.Resize(ref _output, _output.Length + 1);
-                    _output[^1] = words[i];
-                    substring = _output[^1];
-                }
-            }
-        }
+    //         for (int i = 1; i < words.Length; i++)
+    //         {
+    //             substring += " " + words[i];
+    //             if (substring.Length <= 50)
+    //             {
+    //                 _output[^1] = substring;
+    //             }
+    //             else
+    //             {
+    //                 Array.Resize(ref _output, _output.Length + 1);
+    //                 _output[^1] = words[i];
+    //                 substring = _output[^1];
+    //             }
+    //         }
+    //     }
 
-        private string AddTrimInString(string sentence)
-        {
-            int len = sentence.Length;
-            if (len == 50 || len == 0)
-            {
-                return sentence;
-            }
+    //     private string AddTrimInString(string sentence)
+    //     {
+    //         int len = sentence.Length;
+    //         if (len == 50 || len == 0)
+    //         {
+    //             return sentence;
+    //         }
 
-            string[] words = sentence.Split(' ');
-            int cntGap = words.Length - 1;
+    //         string[] words = sentence.Split(' ');
+    //         int cntGap = words.Length - 1;
 
-            if (cntGap == 0)
-            {
-                return sentence;
-            }
-            string ans = words[0];
+    //         if (cntGap == 0)
+    //         {
+    //             return sentence;
+    //         }
+    //         string ans = words[0];
 
-            int cntNecesSpace = 50 - len;
-            int SpaceOnEveryWord = cntNecesSpace / cntGap;
+    //         int cntNecesSpace = 50 - len;
+    //         int SpaceOnEveryWord = cntNecesSpace / cntGap;
 
-            for (int i = 1; i < words.Length; i++)
-            {
-                ans += AddSpaces(SpaceOnEveryWord + 1); 
-                if (i - 1 < cntNecesSpace % cntGap)
-                {
-                    ans += " ";
-                }
-                ans += words[i];
-            }
-            return ans;
-        }
+    //         for (int i = 1; i < words.Length; i++)
+    //         {
+    //             ans += AddSpaces(SpaceOnEveryWord + 1); 
+    //             if (i - 1 < cntNecesSpace % cntGap)
+    //             {
+    //                 ans += " ";
+    //             }
+    //             ans += words[i];
+    //         }
+    //         return ans;
+    //     }
 
-        private string AddSpaces(int cnt)
-        {
-            string ans = "";
-            for (int i = 0; i < cnt; i++)
-            {
-                ans += " ";
-            }
-            return ans;
-        }
+    //     private string AddSpaces(int cnt)
+    //     {
+    //         string ans = "";
+    //         for (int i = 0; i < cnt; i++)
+    //         {
+    //             ans += " ";
+    //         }
+    //         return ans;
+    //     }
 
-        public override void Review()
-        {
-            DoWordsFromInput();
-            _output = _output.Select(AddTrimInString).ToArray();
-        }
+    //     public override void Review()
+    //     {
+    //         DoWordsFromInput();
+    //         _output = _output.Select(AddTrimInString).ToArray();
+    //     }
 
         
-    }
+    // }
