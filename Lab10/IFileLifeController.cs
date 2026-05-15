730dbb1c5698e0b9446ab7cc18cc4b616227bb5f
ddarina <126961692+mitarayushka@users.noreply.github.com>
2026-05-15 19:27:26 +0300

    bug fix

--- Lab10/IFileLifeController.cs
+++ Lab10/IFileLifeController.cs
@@ -2,15 +2,9 @@ namespace Lab10
 {
     public interface IFileLifeController
     {
-         void SelectFolder(string folderName);
-         void CreateFile();
-         void DeleteFile();
-         void EditFile(string content);
-         void ChangeFileExtension(string newExtension);
-         void CreateFolder(string folderName);
-    
-        // void CreateFile();
-        // void DeleteFile();
-        // void RenameFile(string path);
+        void CreateFile();
+        void DeleteFile();
+        void EditFile(string content);
+        void ChangeFileExtension(string extension);
     }
 }
