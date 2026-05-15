e08a65d375f6255038d2d910aac2cb7b43e36825
ddarina <126961692+mitarayushka@users.noreply.github.com>
2026-05-15 19:28:04 +0300

    bug fix

--- Lab10/MyFileManager.cs
+++ Lab10/MyFileManager.cs
@@ -1,116 +1,50 @@
-// using System;
-// using System.IO;
-// using Lab10;
-// namespace Lab10;
-// // public interface IFileManager 
-// // {
-// //     string FolderPath { get; }
-// //     string FileName { get; }
-// //     string FileExtension { get; }
-// //     string FullPath { get; }
-// //     void SelectFolder(string path);
-// //     void ChangeFileName(string name);
-// //     void ChangeFileFormat(string extension);
-// // }
-// //
-// // public interface IFileLifeController 
-// // {
-// //     void CreateFile();
-// //     void DeleteFile();
-// //     void EditFile(string content);
-// //     void ChangeFileExtension(string newExtension);
-// // }
-//
-// public abstract class MyFileManager : IFileManager, IFileLifeController 
-// {
-//     private string _folderPath;
-//     private string _fileName;
-//     private string _fileExtension;
-//     private string _name; 
-//
-//     public MyFileManager(string name) => _name = name;
-//     public MyFileManager(string name, string folder, string file, string ext = "") 
-//     {
-//         _name = name;
-//         _folderPath = folder;
-//         _fileName = file;
-//         _fileExtension = ext;
-//     }
-//
-//     public string FolderPath => _folderPath;
-//     public string FileName => _fileName;
-//     public string FileExtension => _fileExtension;
-//     public string FullPath => Path.Combine(_folderPath ?? "", (_fileName ?? "") + (_fileExtension ?? ""));
-//
-//     public void SelectFolder(string path) => _folderPath = path;
-//     public void ChangeFileName(string name) => _fileName = name;
-//     public void ChangeFileFormat(string extension) => _fileExtension = extension;
-//
-//     public void CreateFile() 
-//     {
-//         if (!string.IsNullOrEmpty(FolderPath)) Directory.CreateDirectory(FolderPath);
-//         File.WriteAllText(FullPath, "");
-//     }
-//
-//     public void DeleteFile() {
-//         if (File.Exists(FullPath)) File.Delete(FullPath);
-//     }
-//
-//     public abstract void EditFile(string content);
-//     public abstract void ChangeFileExtension(string newExtension);
-// }
+using System.IO;
 
-
-namespace Lab10;
-
-public abstract class MyFileManager : IFileManager, IFileLifeController
+namespace Lab10
 {
-    protected string _name;
-    protected string _folderPath;
-    protected string _fileName;
-    protected string _fileExtension;
-
-    // Конструктор
-    public MyFileManager(string name)
+    public abstract class MyFileManager : IFileManager, IFileLifeController
     {
-        _fileName = name;
-        _folderPath = System.IO.Directory.GetCurrentDirectory();
-        _fileExtension = "";
+        protected string _name;
+        protected string _folderPath;
+        protected string _fileName;
+        protected string _fileExtension;
+
+        public string Name => _name;
+        public string FolderPath => _folderPath;
+        public string FileName => _fileName;
+        public string FileExtension => _fileExtension;
+        public string FullPath => Path.Combine(_folderPath ?? "", $"{_fileName}.{_fileExtension}");
+
+        public MyFileManager(string name)
+        {
+            _name = name;
+        }
+
+        public MyFileManager(string name, string folder, string fileName, string ext = "txt")
+        {
+            _name = name;
+            _folderPath = folder;
+            _fileName = fileName;
+            _fileExtension = ext;
+        }
+
+        public void SelectFolder(string path) => _folderPath = path;
+        public void ChangeFileName(string name) => _fileName = name;
+        public void ChangeFileFormat(string extension) => _fileExtension = extension;
+
+        public virtual void CreateFile()
+        {
+            if (!string.IsNullOrEmpty(_folderPath) && !Directory.Exists(_folderPath))
+                Directory.CreateDirectory(_folderPath);
+            File.Create(FullPath).Close();
+        }
+
+        public virtual void DeleteFile()
+        {
+            if (File.Exists(FullPath)) File.Delete(FullPath);
+        }
+
+        public abstract void EditFile(string content);
+        public abstract void ChangeFileExtension(string extension);
     }
-
-    public MyFileManager(string name, string folder, string file, string ext)
-    {
-        _name = name;
-        _fileName = file;
-        _folderPath = folder;
-        _fileExtension = ext;
-    }
-
-    // Свойства из интерфейса IFileManager
-    public string FolderPath => _folderPath;
-    public string FileName => _fileName;
-    public string FileExtension => _fileExtension;
-    public string FullPath => System.IO.Path.Combine(_folderPath, _fileName + _fileExtension);
-
-    // Реализация методов IFileLifeController
-    public void CreateFolder(string path) 
-    {
-        _folderPath = path;
-        if (!System.IO.Directory.Exists(path)) System.IO.Directory.CreateDirectory(path);
-    }
-
-    public void SelectFolder(string path) => _folderPath = path;
-
-    public void ChangeFileName(string name) => _fileName = name;
-
-    public void ChangeFileFormat(string extension) 
-    {
-        _fileExtension = extension.StartsWith(".") ? extension : "." + extension;
-    }
-
-    // Эти методы можно оставить пустыми или реализовать базу
-    public virtual void CreateFile() { }
-    public virtual void DeleteFile() { }
-    public virtual void EditFile(string content) { }
-    public virtual void ChangeFileExtension(string newExtension) => ChangeFileFormat(newExtension);
 }
