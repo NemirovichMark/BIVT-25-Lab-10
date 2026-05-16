3d725547c4ada95121abc3cd9c416aa695e9a2e0
Yokiow <Arishka.drachenko@mail.ru>
2026-05-16 22:30:06 +0300

    Add files via upload

---
+++ Lab10/Green/GreenFileManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10.Green
+{
+    public abstract class GreenFileManager : MyFileManager, ISerializer
+    {
+        public GreenFileManager(string name, string folderPath, string fileName, string fileExtension = "txt") : base(name, folderPath, fileName, fileExtension)
+        {
+        }
+        public GreenFileManager(string name) : base(name)
+        {
+        }
+        public override void EditFile(string newFileText)
+        {
+            File.Exists(FullPath);
+            base.EditFile(newFileText);
+        }
+
+        public override void ChangeFileExtension(string newFileExtension)
+        {
+            File.Exists(FullPath);
+            base.ChangeFileExtension(newFileExtension);
+        }
+
+        abstract public void Serialize<T>(T obj) where T : Lab9.Green.Green;
+        abstract public T Deserialize<T>() where T : Lab9.Green.Green;
+    }
+}
