731c0fb04c933f4cf6f320628cb683d722a325a4
sss <d>
2026-05-16 03:53:12 +0300

    fix

--- Lab9/obj/Debug/net8.0/Lab9.AssemblyInfo.cs
+++ Lab9/obj/Debug/net8.0/Lab9.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lab10")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lab9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e6f541613dca46d8d18c4fc5e117ba370458af6")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lab10")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lab10")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lab9")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lab9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
