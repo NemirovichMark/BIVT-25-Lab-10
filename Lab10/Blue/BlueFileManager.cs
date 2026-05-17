54fad8c2e04332945adadf3b893bcd5d36b87fbc
agrowow <ivan.pechersky07@icloud.com>
2026-05-17 15:22:18 +0300

    BlueFileManager.cs

--- Lab10/Blue/BlueFileManager.cs
+++ Lab10/Blue/BlueFileManager.cs
@@ -1,7 +1,10 @@
 using System.IO;
+using Lab9.Blue;
+
 namespace Lab10.Blue
 {
-    public abstract class BlueFileManager<T> : MyFileManager, ISerializer<T> where T : Blue
+    public abstract class BlueFileManager<T> : MyFileManager, ISerializer<T>
+        where T : Blue
     {
         protected BlueFileManager(string name) : base(name) { }
         protected BlueFileManager(string name, string folder, string file, string ext = "txt") 
