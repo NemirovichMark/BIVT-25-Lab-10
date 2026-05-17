23aa6650f1bbf7265c6d2418b26dabdd26b7e4b0
DashaSr <sergeevada07@mail.ru>
2026-05-17 20:15:54 +0300

    Update MyFileManager.cs

--- Lab10/Lab10.blue/MyFileManager.cs
+++ Lab10/Lab10.blue/MyFileManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab10.Lab10.Blue
+namespace Lab10.Blue
 {
     public abstract class MyFileManager : IFileManager, IFileLifeController
     {
