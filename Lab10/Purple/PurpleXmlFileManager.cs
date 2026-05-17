627902987475f4ec62716788e945d1b04b950830
Vladimir <vmatusyak07@gmail.com>
2026-05-18 00:02:01 +0300

    Update PurpleXmlFileManager.cs

--- Lab10/Purple/PurpleXmlFileManager.cs
+++ Lab10/Purple/PurpleXmlFileManager.cs
@@ -5,71 +5,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
-using System.Reflection;
 
 namespace Lab10.Purple
 {
-    public class PurpleXmlFileManager<T> : PurpleFileManager<T>
-        where T : Lab9.Purple.Purple
+    public class PurpleXmlFileManager<T> : PurpleFileManager<T> where T : Lab9.Purple.Purple
     {
         public PurpleXmlFileManager(string name) : base(name) { }
+        public PurpleXmlFileManager(string name, string folderPath, string fileName, string fileExtension = "txt")
+            : base(name, folderPath, fileName, fileExtension) { }
 
-        public PurpleXmlFileManager(string name, string folderPath, string fileName, string fileExtension = "txt") : base(name, folderPath, fileName, fileExtension) { }
-
-        public override void EditFile(string content)
+        public override void EditFile(string text)
         {
             T obj = Deserialize();
-            if (obj != null)
-            {
-                obj.ChangeText(content);
-                Serialize(obj);
-            }
+            obj.ChangeText(text);
+            Serialize(obj);
         }
         public override void ChangeFileExtension(string extension)
         {
-            T obj = Deserialize();
-            ChangeFileFormat("xml");
-            if (obj != null)
-                Serialize(obj);
+            base.ChangeFileExtension("xml");
         }
         public override void Serialize(T obj)
         {
-            if (obj != null && FullPath != null)
+            if (obj == null) return;
+
+            DTOPurple dtopurple = new DTOPurple(obj);
+
+            var serializer = new XmlSerializer(typeof(DTOPurple));
+            using (var writer = new StreamWriter(FullPath))
             {
-                var d = new DTOPurple(obj);
-                var serializer = new XmlSerializer(typeof(DTOPurple));
-                using (var sw = new StreamWriter(FullPath))
-                    serializer.Serialize(sw, d);
+                serializer.Serialize(writer, dtopurple);
             }
         }
-
         public override T Deserialize()
         {
+            if (!File.Exists(FullPath)) return null;
+
+            DTOPurple objDTO;
+            T obj = null;
+
             var serializer = new XmlSerializer(typeof(DTOPurple));
-            if (string.IsNullOrEmpty(FullPath) || !File.Exists(FullPath)) return null;
-            using (var sr = new StreamReader(FullPath))
+            using (var reader = new StreamReader(FullPath))
             {
-                var d = (DTOPurple)serializer.Deserialize(sr);
-                Lab9.Purple.Purple result;
-                switch (d.TypeName)
-                {
-                    case "Task1":
-                        result = new Task1(d.Input);
-                        break;
-                    case "Task2":
-                        result = new Task2(d.Input);
-                        break;
-                    case "Task3":
-                        result = new Task3(d.Input);
-                        break;
-                    case "Task4":
-                        result = new Task4(d.Input, d.GetDecodedCodes());
-                        break;
-                    default:
-                        return null;
-                }
-                result.Review();
-                return (T)result;
+                objDTO = (DTOPurple)serializer.Deserialize(reader);
+            }
+            if (objDTO == null) return null; 
+
+            if (objDTO.Type == "Task1")
+            {
+                obj = (new Task1(objDTO.Input) as T);
+            }
+            else if (objDTO.Type == "Task2")
+            {
+                obj = (new Task2(objDTO.Input) as T);
+            }
+            else if (objDTO.Type == "Task3")
+            {
+                obj = (new Task3(objDTO.Input) as T);
+            }
+            else if (objDTO.Type == "Task4")
+            {
+                obj = (new Task4(objDTO.Input, objDTO.CodesT4) as T);
+            }
+            
+            if (obj == null) return null;
+            obj.Review();
+
+            return obj;
+        }
+    }
+    public class DTOPurple
+    {
+        public string Type { get; set; }
+        public string Input { get; set; }
+        public (string, char)[] CodesT4 { get; set; }
+        public DTOPurple() { }
+        public DTOPurple(Lab9.Purple.Purple obj)
+        {
+            Type = obj.GetType().Name;
+            Input = obj.Input;
+            if (obj is Task4 o)
+            {
+                CodesT4 = o.Codes;
             }
         }
     }
