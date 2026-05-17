1ebc19b54f000cc2af1b9f16cd108b36fcb68d8b
AlenaNovakh <alenanovakh@yandex.ru>
2026-05-17 20:39:32 +0300

    Update IFileManager.cs

--- Lab10/White/IFileManager.cs
+++ Lab10/White/IFileManager.cs
@@ -8,5 +8,13 @@ namespace Lab10
 {
     internal interface IFileManager
     {
+        string FolderPath { get; }
+        string FileName { get; }
+        string FileExtension { get; }
+        string FullPath { get; }
+
+        void SelectFolder(string folderPath);
+        void ChangeFileName(string fileName);
+        void ChangeFileFormat(string extension);
     }
 }
