12a4e8a98fa9afe261439bff1f776c430e7e3887
AlenaNovakh <alenanovakh@yandex.ru>
2026-05-17 21:58:35 +0300

    Update White.cs

--- Lab9/White/White.cs
+++ Lab9/White/White.cs
@@ -1,1 +1,25 @@
+namespace Lab9.White
+{
+    public abstract class White
+    {
+        private string _input;
+        public string Input => _input;
 
+        protected White(string input)
+        {
+            _input = input;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+        public virtual string GetDataType()
+        {
+            return GetType().Name;
+        }
+    }
+}
