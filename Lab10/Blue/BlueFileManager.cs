a9dae535c40a1bf31ed1add3f8fb07339f18be69
agrowow <ivan.pechersky07@icloud.com>
2026-05-17 15:36:48 +0300

    BlueFileManager.cs

--- Lab10/Blue/BlueFileManager.cs
+++ Lab10/Blue/BlueFileManager.cs
@@ -1,32 +1,44 @@
 using System.IO;
-using Lab9.Blue;
 
 namespace Lab10.Blue
 {
     public abstract class BlueFileManager<T> : MyFileManager, ISerializer<T>
-        where T : Blue
+        where T : Lab9.Blue.Blue
     {
-        protected BlueFileManager(string name) : base(name) { }
-        protected BlueFileManager(string name, string folder, string file, string ext = "txt") 
-            : base(name, folder, file, ext) { }
+        protected BlueFileManager(string name) : base(name)
+        {
+        }
+
+        protected BlueFileManager(string name, string folder, string file, string ext = "txt")
+            : base(name, folder, file, ext)
+        {
+        }
 
         public override void EditFile(string content)
         {
             if (File.Exists(FullPath))
             {
-                var obj = Deserialize();
-                obj?.ChangeText(content);
-                Serialize(obj);
+                T obj = Deserialize();
+
+                if (obj != null)
+                {
+                    obj.ChangeText(content);
+
+                    Serialize(obj);
+                }
             }
         }
 
         public override void ChangeFileExtension(string extension)
         {
             if (!string.IsNullOrEmpty(extension))
+            {
                 ChangeFileFormat(extension);
+            }
         }
 
         public abstract void Serialize(T obj);
+
         public abstract T Deserialize();
     }
 }
