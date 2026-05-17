496669e1bb1e3a1254d13f8cd11f38f73c7f7b83
Vladimir <vmatusyak07@gmail.com>
2026-05-17 17:03:27 +0300

    Update DTOPurple.cs

--- Lab10/Purple/DTOPurple.cs
+++ Lab10/Purple/DTOPurple.cs
@@ -1,44 +1,35 @@
-using System.Xml.Serialization;
-
-namespace Lab10.Purple;
-
-[XmlRoot("DTOPurple")]
-public class DTOPurple
+namespace Lab10.Purple
 {
-    [XmlElement("TypeName")]
-    public string TypeName { get; set; }
-
-    [XmlElement("Input")]
-    public string Input { get; set; }
-
-    [XmlArray("Items")]
-    [XmlArrayItem("Item")]
-    public DTOItem[] Items { get; set; }
-
-    public DTOPurple() { }
-
-    public DTOPurple(string typeName, string input, DTOItem[] items = null)
+    public class DTOPurple
     {
-        TypeName = typeName;
-        Input = input;
-        Items = items ?? Array.Empty<DTOItem>();
-    }
-}
+        public string TypeName { get; set; }
+        public string Input { get; set; }
+        public string[] Codes { get; set; }
 
-[XmlType("Item")]
-public class DTOItem
-{
-    [XmlElement("Key")]
-    public string Key { get; set; }
+        public DTOPurple() { }
 
-    [XmlElement("Value")]
-    public char Value { get; set; }
+        public DTOPurple(Lab9.Purple.Purple obj)
+        {
+            TypeName = obj.GetType().Name;
+            Input = obj.Input;
+            if (obj is Lab9.Purple.Task4 task4 && task4.Codes != null)
+            {
+                Codes = new string[task4.Codes.Length];
+                for (int i = 0; i < Codes.Length; i++)
+                    Codes[i] = task4.Codes[i].Item1 + "|" + task4.Codes[i].Item2;
+            }
+        }
 
-    public DTOItem() { }
-
-    public DTOItem(string key, char value)
-    {
-        Key = key;
-        Value = value;
+        internal (string, char)[] GetDecodedCodes()
+        {
+            if (Codes == null) return Array.Empty<(string, char)>();
+            var result = new (string, char)[Codes.Length];
+            for (int i = 0; i < Codes.Length; i++)
+            {
+                int idx = Codes[i].LastIndexOf('|');
+                result[i] = (Codes[i].Substring(0, idx), Codes[i][idx + 1]);
+            }
+            return result;
+        }
     }
 }
