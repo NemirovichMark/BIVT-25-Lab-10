a2fd77a2b46b5437e343af5ec38da9864eab9be6
Vladimir <vmatusyak07@gmail.com>
2026-05-17 23:00:01 +0300

    Update PurpleFileManager.cs

--- Lab10/Purple/PurpleFileManager.cs
+++ Lab10/Purple/PurpleFileManager.cs
@@ -3,27 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Lab10.Purple
 {
-    public abstract class PurpleFileManager<T> : MyFileManager, ISerializer<T> where T: Lab9.Purple.Purple
+    public abstract class PurpleFileManager<T> : MyFileManager, ISerializer<T>
+         where T : Lab9.Purple.Purple
     {
-        public PurpleFileManager(string name) : base (name) { }
-        public PurpleFileManager(string name, string folderPath, string fileName, string fileExtension = "txt")
-            : base (name, folderPath, fileName, fileExtension) { }
+        public PurpleFileManager(string name) : base(name) { }
 
-        public override void EditFile(string text)
+        public PurpleFileManager(string name, string folderName, string fileName, string fileExtension = "txt") : base(name, folderName, fileName, fileExtension) { }
+
+        public override void EditFile(string content) 
         {
-            if (!File.Exists(FullPath)) return;
-            base.EditFile(text);
+            if (File.Exists(FullPath) && !string.IsNullOrEmpty(FullPath))
+            {
+                base.EditFile(content);
+            }
         }
         public override void ChangeFileExtension(string extension)
         {
-            if (!File.Exists(FullPath)) return;
-            base.ChangeFileExtension(extension);
+            if (File.Exists(FullPath) && Path.Exists(FolderPath))
+            {
+                string content = File.ReadAllText(FullPath);
+                string oldPath = FullPath;
+                ChangeFileFormat(extension);
+                File.WriteAllText(FullPath, content);
+                File.Delete(oldPath);
+            }
         }
-        public abstract void Serialize(T obj);
+
         public abstract T Deserialize();
+        public abstract void Serialize(T obj);
     }
 }
